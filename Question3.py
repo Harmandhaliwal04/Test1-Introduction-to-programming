e4934ed49c7bfb31970956adf506ba697c9d5792
jotdhessi <harmanjot28082004@gmail.com>
2023-11-29 16:30:41 -0500

    version8

---
+++ Question3.py
@@ -0,0 +1,15 @@
+a=input("Enter the country ")
+b=float(input("Enter the salary "))
+
+
+
+if(a=="canada"):
+    con=b*.86
+    if(con>56000):
+        print("It is good to come in canada")
+    else:
+        print("He may be poor ")   
+
+
+
+
