329d15eee70e2a17e17a47c8366f4a86c324a2d7
jotdhessi <harmanjot28082004@gmail.com>
2023-11-29 15:08:45 -0500

    version4

---
+++ Question2.py
@@ -0,0 +1,19 @@
+Color1=input("Enter the Color1")
+color2=input("Enter the color2")
+
+
+if("color1=red ,color1=blue , color1=yellow") :
+     print()
+else:
+    print("Invalid color")
+
+
+if("color2=Red , color2=blue , color1=yellow"):
+    print()
+else:
+    print("Invalid color")
+          
+if ("color1==Red"):
+ if("Color2==Blue"):
+     print("Orange")
+
