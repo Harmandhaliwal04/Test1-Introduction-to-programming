a25e8f32c2f36d9b576923b02283e9a590961835
jotdhessi <harmanjot28082004@gmail.com>
2023-11-29 15:21:34 -0500

    version5

--- Question2.py
+++ Question2.py
@@ -1,19 +1,35 @@
 Color1=input("Enter the Color1")
-color2=input("Enter the color2")
+Color2=input("Enter the color2")
 
 
-if("color1=red ,color1=blue , color1=yellow") :
-     print()
+if(Color1=="red" or Color1=="yellow" or Color1=="blue") :
+    print()
 else:
     print("Invalid color")
 
 
-if("color2=Red , color2=blue , color1=yellow"):
+if(Color2=="red" or Color2=="yellow" or Color2=="blue") :
     print()
 else:
     print("Invalid color")
           
-if ("color1==Red"):
- if("Color2==Blue"):
-     print("Orange")
+if(Color1=="red") :
+    if(Color2=="blue"):
+        print("Purple")
+
+    if(Color2=="yellow"):
+        print("Orange")
+elif(Color1=="blue") :
+    if(Color2=="red"):
+        print("Purple")
+
+    if(Color2=="yellow"):
+        print("Orange")
+
+elif(Color1=="yellow") :
+    if(Color2=="red"):
+        print("Orange")
+
+    if(Color2=="yellow"):
+        print("Green")        
 
